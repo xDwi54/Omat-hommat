85515796fdfa97dc32454cd20eceae2c776074b2
xDwi54 <60254669+xDwi54@users.noreply.github.com>
2020-08-20 10:48:20 +0300

    Add files via upload

--- kansio/java.js
+++ kansio/java.js
@@ -34,38 +34,107 @@ function vertaile()
         document.write(luku2 + ' on isompi kuin ' + luku1);
     }
 }
-
-function lahetaVastaukset()
+function kaanna()
 {
-    var yhteensa = 2;
-    var pisteet = 0;
-    var huom = 0;
-
-    var k1 = document.forms["kyselyLomake"] ["k1"].value;
-    var k2 = document.forms["kyselyLomake"] ["k2"].value;
-
-    for (i = 1; i <= yhteensa; i++)
-    {
-        if(eval('k'+i) == null || eval('k'+i) == '')
-        {
-            alert('Et vastannut kysymykseen numero: ' + i);
-            huom = 1;
-        }
-        if(huom == 1) return false;
-    }
-
-    var vastaukset = ["b", "d"];
-
-    for(i = 1 ; i <= yhteensa; i++)
-    {
-        if(eval('k'+i) == vastaukset[i-1])
-        {
-            pisteet++;
-        }
-    }
-
-    var tulokset = document.getElementById('tulokset');
-    alert('Sait ' + pisteet + ' pistettä, kun maksimi pistemäärä oli' + yhteensa);
-    tulokset.innerHTML = '<h3>Sait <span>' + pisteet + '</span> pistettä, kun maksimi pistemäärä oli <span>' +yhteensa + '</span> </h3>';
-    return false;
+  var lang = document.getElementById('kieli').value;
+  if(lang == 'eng')
+  {
+    document.getElementById('jmj').innerHTML = "Hello world!";
+  }
+  else if(lang == 'swe')
+  {
+    document.getElementById('jmj').innerHTML = "Hej världen!";
+  }
+  else {
+    document.getElementById('jmj').innerHTML = "Hola Mundo!";
+  }
 }
+function ajoneuvo()
+{
+    var ika = document.getElementById('ika').value;
+    if(ika < 15)
+    {
+      document.getElementById('vc').innerHTML = " Saat ajaa polkupyörällä";
+    }
+    else if(ika > 15 && ika < 18)
+    {
+      document.getElementById('vc').innerHTML = "Saat ajaa mopoa";
+    }
+    else {
+      document.getElementById('vc').innerHTML = "Saat ajaa autoa";
+    }
+}
+function pariton()
+{
+    document.getElementById('vhu').innerText = ((parseInt(document.getElementById('luku').value) % 2 == 0) ? 'Kyllä' : 'Ei');
+}
+function etsiSuurin()
+  {
+    var l1, l2, l3, l4, l5, suurin;
+    l1 = parseInt(document.getElementById('luku1').value);
+    l2 = parseInt(document.getElementById('luku2').value);
+    l3 = parseInt(document.getElementById('luku3').value);
+    l4 = parseInt(document.getElementById('luku4').value);
+    l5 = parseInt(document.getElementById('luku5').value);
+    if(l1 < l2 && l3 < l2 && l4 < l2 && l5 < l2)
+    {
+      suurin = l2;
+    }
+    else if(l1 < l3 && l4 < l3 && l5 < l3)
+    {
+      suurin = l3;
+    }
+    else if(l1 < l4 && l5 < l4)
+    {
+      suurin = l4;
+    }
+    else if(l1 < l5)
+    {
+      suurin = l5;
+    }
+    else {
+      suurin = l1;
+    }
+    document.getElementById('fyt').innerHTML = "Annoit luvut: " + l1 + ' ' + l2 + ' ' + l3 + ' ' + l4 + ' ' + l5;
+    document.getElementById('mnjn').innerHTML = "Suurin niistä on: " + suurin;
+  }
+  function jarjesta()
+  {
+    var eluku1, eluku2, eluku3;
+    eluku1 = parseInt(document.getElementById('eeka').value);
+    eluku2 = parseInt(document.getElementById('etoka').value);
+    eluku3 = parseInt(document.getElementById('ekolmas').value);
+    document.write("Annoit luvut: " + eluku1 + " " + eluku2 + " " + eluku3 + "<br>");
+    if(eluku1 < eluku2 && eluku1 < eluku3)
+    {
+      if(eluku2 < eluku3)
+      {
+        document.getElementById('fh').innerHTML = "Lukujen järjestys: " + eluku1 + ' ' + eluku2 + ' ' + eluku3;
+      }
+      else {
+        document.getElementById('fhf').innerHTML = " + eluku1 + ' ' + eluku3 + ' ' + eluku2";
+      }
+    }
+    else if(eluku2 < eluku1 && eluku2 < eluku3)
+    {
+      if(eluku1 < eluku3)
+      {
+        document.getElementById('fhd').innerHTML = " + eluku2 + ' ' + eluku1 + ' ' + eluku3";
+      }
+      else {
+        document.getElementById('fhl').innerHTML = " + eluku2 + ' ' + eluku3 + ' ' + eluku1";
+  
+      }
+    }
+    else if(eluku3 < eluku1 && eluku3 < eluku2)
+      {
+        if(eluku1 < eluku2)
+        {
+            document.getElementById('fhk').innerHTML = " + eluku3 + ' ' + eluku1 + ' ' + eluku2";
+        }
+        else {
+            document.getElementById('fhj').innerHTML = " + eluku3 + ' ' + eluku2 + ' ' + eluku1";
+  
+        }
+      }
+    }
