0013b909ea93ec552ab5d60e385e1024063169ea
xDwi54 <60254669+xDwi54@users.noreply.github.com>
2020-10-26 11:19:33 +0200

    Add files via upload

---
+++ C-Sharp Harjotuksia/Harjotuksia/Harjotuksia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Harjotuksia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            nimi();
+        }
+        public static void nimi()
+        {
+            Console.WriteLine("Syötä Nimesi");
+            string nimi = Console.ReadLine();
+            Console.WriteLine("Hei " + nimi);
+        }
+    }
+}
